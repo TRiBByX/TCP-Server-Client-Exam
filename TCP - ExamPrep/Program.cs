80e37321423c340a35b6b0a6d1801a6078b2d1b2
TRiBB <TRiBB@WORK>
2017-01-09 04:03:56 +0100

    Made a tcp server and client

--- TCP - ExamPrep/Program.cs
+++ TCP - ExamPrep/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,30 @@ namespace TCP___ExamPrep
     {
         static void Main(string[] args)
         {
+            //Server
+            TcpListener serverSocket = new TcpListener(8989);
+            serverSocket.Start();
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Server Activated");
+
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                string message = sr.ReadLine();
+                Console.WriteLine($"Client: {message}");
+                string answer = Console.ReadLine();
+                sw.WriteLine(answer);
+            }
+
+            string s = Console.ReadLine();
+
+            ns.Close();
+            connectionSocket.Close();
+            serverSocket.Stop();
         }
     }
 }
---
+++ TCP Client  - ExamPrep/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TCP_Client____ExamPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient connectionSocket = new TcpClient("localhost", 8989);
+
+            Stream ns = connectionSocket.GetStream();
+            StreamWriter sw = new StreamWriter(ns);
+            StreamReader sr = new StreamReader(ns);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                sw.WriteLine(message);
+                string answer = sr.ReadLine();
+                Console.WriteLine($"Server: {message}");
+            }
+
+            string s = Console.ReadLine();
+
+            ns.Close();
+            connectionSocket.Close();
+        }
+    }
+}
